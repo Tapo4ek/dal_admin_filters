e1c1715721f1ca1b665a3234bbc79270271ae525
Tapo4ek <infdods@yandex.ru>
2017-11-03 01:09:37 +0300

    Related lookup

--- dal_admin_filters/__init__.py
+++ dal_admin_filters/__init__.py
@@ -4,6 +4,8 @@ from django import forms
 from django.contrib.admin.filters import SimpleListFilter
 from django.core.exceptions import ImproperlyConfigured
 from django.forms.widgets import Media, MEDIA_TYPES
+from django.apps import apps
+from dal import autocomplete
 
 
 class AutocompleteFilter(SimpleListFilter):
@@ -11,6 +13,8 @@ class AutocompleteFilter(SimpleListFilter):
     title = ''
     field_name = ''
     autocomplete_url = ''
+    autocomplete_model = ''
+    query_attr_name = ''
     is_placeholder_title = False
     widget_attrs = {}
 
@@ -36,13 +40,17 @@ class AutocompleteFilter(SimpleListFilter):
                 'Rename attribute `parameter_name` to '
                 '`field_name` for {}'.format(self.__class__)
             )
-        self.parameter_name = '{}__id__exact'.format(self.field_name)
+        self.parameter_name = self.query_attr_name or '{}__id__exact'.format(self.field_name)
         super(AutocompleteFilter, self).__init__(request, params, model, model_admin)
 
         self._add_media(model_admin)
 
+        if self.autocomplete_model:
+            queryset = apps.get_model(*self.autocomplete_model.split('.', 1))._meta.default_manager.all()
+        else:
+            queryset = getattr(model, self.field_name).get_queryset()
         field = forms.ModelChoiceField(
-            queryset=getattr(model, self.field_name).get_queryset(),
+            queryset=queryset,
             widget=autocomplete.ModelSelect2(
                 url=self.autocomplete_url,
             )
